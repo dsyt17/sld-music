b00a95baff3933c47ae993553ea7da109784ddfc
Your Name <goncharovigor12@gmail.com>
2023-04-14 23:07:04 +0300

    about page

--- src/routes/About/About.component.tsx
+++ src/routes/About/About.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import classNames from 'classnames';
 import { motion } from 'framer-motion';
 
 import styles from './About.module.scss';
@@ -8,24 +9,84 @@ const About = () => {
     return (
         <div className={styles.root}>
             <div className={styles.header}>
-                <motion.div
-                    initial={{ x: '200%', opacity: 0 }}
-                    animate={{ x: 0, opacity: 1 }}
-                // transition={{ delay: 0.2, duration: 0.5 }}
-                >
+                <motion.div initial={{ x: '200%', opacity: 0 }} animate={{ x: 0, opacity: 1 }}>
                     SLD Music
                 </motion.div>
             </div>
-            <motion.div
-                className={styles.title}
-                initial={{ x: '-200%', opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-            // transition={{ delay: 0.2, duration: 0.5 }}
-            >
+            <Text>
                 Творческое объединение молодых музыкантов основанное в 2017 году и создающую музыку
-                различных жанров.
-            </motion.div>
+                различных жанров. Творческое объединение молодых музыкантов основанное в 2017 году и
+                создающую музыку различных жанров. Творческое объединение молодых музыкантов
+                основанное в 2017 году и создающую музыку различных жанров.Творческое объединение
+                молодых музыкантов основанное в 2017 году и создающую музыку различных
+                жанров.Творческое объединение молодых музыкантов основанное в 2017 году и создающую
+                музыку различных жанров.
+            </Text>
+            <Card direction="left">Тут картинки или чето крутое</Card>
+            <Text>
+                Творческое объединение молодых музыкантов основанное в 2017 году и создающую музыку
+                различных жанров. Творческое объединение молодых музыкантов основанное в 2017 году и
+                создающую музыку различных жанров. Творческое объединение молодых музыкантов
+                основанное в 2017 году и создающую музыку различных жанров.Творческое объединение
+                молодых музыкантов основанное в 2017 году и создающую музыку различных
+                жанров.Творческое объединение молодых музыкантов основанное в 2017 году и создающую
+                музыку различных жанров.
+            </Text>
+            <Card direction="right">Тут картинки или чето крутое</Card>
+            <Text>
+                Творческое объединение молодых музыкантов основанное в 2017 году и создающую музыку
+                различных жанров. Творческое объединение молодых музыкантов основанное в 2017 году и
+                создающую музыку различных жанров. Творческое объединение молодых музыкантов
+                основанное в 2017 году и создающую музыку различных жанров.Творческое объединение
+                молодых музыкантов основанное в 2017 году и создающую музыку различных
+                жанров.Творческое объединение молодых музыкантов основанное в 2017 году и создающую
+                музыку различных жанров.
+            </Text>
+            <Card direction="left">Тут картинки или чето крутое</Card>
         </div>
+    );
+};
+
+const Card: React.FC<{ children: React.ReactNode; direction: 'left' | 'right' }> = ({
+    children,
+    direction,
+}) => {
+    return (
+        <motion.div
+            initial={{
+                x: direction === 'left' ? '-50%' : '50%',
+                opacity: 0,
+            }}
+            whileInView={{
+                x: 0,
+                opacity: 1,
+            }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6, delay: 0.1 }}
+            className={classNames(styles.card, direction === 'left' ? styles.left : styles.right)}
+        >
+            {children}
+        </motion.div>
+    );
+};
+
+const Text: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+        <motion.div
+            initial={{
+                y: 100,
+                opacity: 0,
+            }}
+            whileInView={{
+                y: 0,
+                opacity: 1,
+            }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6 }}
+            className={styles.title}
+        >
+            {children}
+        </motion.div>
     );
 };
 
--- src/routes/Discography/Discography.component.tsx
+++ src/routes/Discography/Discography.component.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
 
+import styles from './Discography.module.scss';
+
 const Discography = () => {
-    return <div>Discography.component</div>;
+    return (
+        <div className={styles.root}>
+            <Divider year={2021} />
+            <ReleaseCarousel>
+                <Release />
+                <Release />
+                <Release />
+            </ReleaseCarousel>
+            <Divider year={2021} />
+            <ReleaseCarousel>
+                <Release />
+                <Release />
+                <Release />
+            </ReleaseCarousel>
+            <Divider year={2021} />
+            <ReleaseCarousel>
+                <Release />
+                <Release />
+                <Release />
+            </ReleaseCarousel>
+        </div>
+    );
+};
+
+const Release = () => {
+    return (
+        <div className={styles.release}>
+            <img alt="Cover" src="./public/assets/secta.jpg" />
+            <div className={styles.title}>Секта</div>
+            <div className={styles.artist}>Yung Sleep</div>
+        </div>
+    );
+};
+
+const ReleaseCarousel = ({ children }) => {
+    return <div className={styles.carousel}>{children}</div>;
+};
+
+const Divider = ({ year }) => {
+    return (
+        <div className={styles.divider}>
+            {year}
+            <div className={styles.dividerLine}></div>
+        </div>
+    );
 };
 
 export default Discography;
